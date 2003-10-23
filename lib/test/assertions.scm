b11c4e38326bcfc3f692dcfdc66d3e66b324f15a
Kouhei Sutou <kou@cozmixng.org>
2003-10-23 10:10:09 +0000

    * Added some assertions.

--- lib/test/assertions.scm
+++ lib/test/assertions.scm
@@ -3,28 +3,67 @@
 
 (define-syntax define-assertion
   (syntax-rules ()
-    ((_ (name arg ...) message body ...)
+    ((_ (name arg ...) message-handler body ...)
      (begin
        (export name)
        (define (name arg ...)
          (if (test-result)
-             (if (begin body ...)
-                 (add-success! (test-result) (test-ui) (current-test))
-                 (add-fail! (test-result) (test-ui) (current-test) message
-                            (list (cadr (vm-get-stack-trace)))))))))))
+             (let ((result (begin body ...)))
+               (apply (if (eq? #t result)
+                          add-success!
+                          add-fail!)
+                      `(,(test-result) ,(test-ui) ,(current-test)
+                        ,@(if (eq? #t result)
+                              '()
+                              (list (message-handler result)
+                                    (list (cadr (vm-get-stack-trace))))))))))))
+    ))
 
 (define-assertion (assert actual)
-  (format "<not #f> expected but got <~s>" actual)
+  (lambda (result)
+    (format "<not #f> expected but got <~s>" result))
   actual)
 
 (define-assertion (assert-equal expected actual)
-  (format "<~s> expected but got <~s>" expected actual)
+  (lambda (result)
+    (format "<~s> expected but got <~s>" expected actual))
   (equal? expected actual))
 
 (define-assertion (assert-true actual)
-  (format "<#t> expected but got <~s>" actual)
+  (lambda (result)
+    (format "<#t> expected but got <~s>" actual))
   (eq? #t actual))
 
 (define-assertion (assert-false actual)
-  (format "<#f> expected but got <~s>" actual)
+  (lambda (result)
+    (format "<#f> expected but got <~s>" actual))
   (not actual))
+
+(define-assertion (assert-instance-of expected-class object)
+  (lambda (result)
+    (format "<~s> expected to be an instance of <~s> but was <~s>"
+            object expected-class (class-of object)))
+  (is-a? object expected-class))
+
+(define-assertion (assert-raise expected-class thunk)
+  (if (is-a? expected-class <class>)
+      (lambda (result)
+        (format "<~s> exception expected but ~a"
+                expected-class result))
+      (lambda (result)
+        (format "Should expect a class of exception")))
+  (if (is-a? expected-class <class>)
+      (with-error-handler
+       (lambda (err)
+         (if (is-a? err expected-class)
+             #t
+             (format "was <~s>" (class-of err))))
+       (lambda () (thunk) "none was thrown"))
+      #f))
+
+(define-assertion (assert-error thunk)
+  (lambda (result)
+    (format "none expection was thrown"))
+  (with-error-handler
+   (lambda (err) #t)
+   (lambda () (thunk))))
--- test/test-assert.scm
+++ test/test-assert.scm
@@ -2,41 +2,41 @@
 
 (use test.unit)
 
-(let ((test (make-test-case "Test assertions"
-             (setup
-              (lambda ()
-                (print "setuped")))
-             (teardown
-              (lambda ()
-                (print "teardownd")))
-             ("Test assert"
-              (assert #t)
-              (assert #f))
-             ("Test assert-equal"
-              (assert-equal 1 (+ 1 2))
-              (assert-equal 3 (+ 1 2))
-              (assert-equal 1 (+ 1 2 -2))))))
+(let ((test
+       (make-test-case "Test assertions"
+         ("Test assert"
+          (assert #t)
+          (assert #f))
+         ("Test assert-equal"
+          (assert-equal 1 (+ 1 2))
+          (assert-equal 3 (+ 1 2))
+          (assert-equal 1 (+ 1 2 -2))))))
   (run test))
 
 (let ((test
        (make-test-suite
         "Test test-suite"
         ("Test case1"
-         (setup
-          (lambda ()
-            (print "case1 setuped")))
          ("Test assert1"
           (assert #t))
          ("Test assert-equal1"
           (assert-equal 1 (+ 1 2))))
         ("Test case2"
-         (teardown
-          (lambda ()
-            (print "case2 teardowned")))
          ("Test assert1"
           (assert #f))
          ("Test assert-equal2"
-          (assert-equal 1 (+ 1 2 -2))))
-        )))
+          (assert-equal 1 (+ 1 2 -2)))
+         ("Test assert-instance-of"
+          (assert-instance-of <string> "a")
+          (assert-instance-of <integer> "a"))
+         ("Test assert-raise"
+          (assert-raise <error> (lambda () 1))
+          (assert-raise <error> (lambda () (1)))
+          (assert-raise 1 (lambda () (1)))
+          )
+         ("Test assert-error"
+          (assert-error (lambda () 1))
+          (assert-error (lambda () (1)))
+         )))))
   (run test))
 
