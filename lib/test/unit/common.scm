f6db3f1512a36c692c608b4238e74091a5ddd9ee
Kouhei Sutou <kou@cozmixng.org>
2004-05-26 12:30:08 +0000

    * forgot add files.

---
+++ lib/test/unit/common.scm
@@ -0,0 +1,12 @@
+(define-module test.unit.common
+  (use gauche.parameter))
+(select-module test.unit.common)
+
+(define *gaunit-version* "0.0.6")
+
+(define test-result (make-parameter #f))
+(define test-ui (make-parameter #f))
+(define current-test (make-parameter #f))
+(define count-assertion (make-parameter #t))
+
+(provide "test/unit/common")
