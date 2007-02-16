4d0a5a07eed69fea7951a0a46cff948ec7ce8340
Kouhei Sutou <kou@cozmixng.org>
2007-02-16 13:32:04 +0000

    * updated key bindings.

--- sample/site-lisp/run-test-setting.el
+++ sample/site-lisp/run-test-setting.el
@@ -3,10 +3,11 @@
 ;; autoload
 (autoload 'run-test "run-test" nil t)
 (autoload 'run-test-in-new-frame "run-test" nil t)
+(autoload 'run-test-in-mini-buffer "run-test" nil t)
 
 ;; key bindings
 (define-key global-map "\C-c\C-t" 'run-test)
 (define-key global-map "\C-cT" 'run-test-in-new-frame)
-(define-key global-map "\C-c\C-c\C-t" 'run-test-in-mini-buffer)
+(define-key global-map "\C-[\M-\C-t" 'run-test-in-mini-buffer)
 
 ;;; end of file
