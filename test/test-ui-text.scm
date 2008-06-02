0e675fe5869c4dc06d28e63690ea79311617da28
Kouhei Sutou <kou@cozmixng.org>
2008-06-02 01:08:44 +0000

    * add a test for empty <test>.

--- test/test-ui-text.scm
+++ test/test-ui-text.scm
@@ -6,12 +6,17 @@
 
 (define empty-test-suite #f)
 (define empty-test-case #f)
+(define empty-test #f)
 
 (define (setup)
   (set! empty-test-suite (make <test-suite> :name "empty test suite"))
-  (set! empty-test-case (make <test-case> :name "empty test case")))
+  (set! empty-test-case (make <test-case> :name "empty test case"))
+  (set! empty-test (make <test> :name "empty test")))
 
 (define (test-empty-test-suite)
+  (assert-output ""
+                 (lambda ()
+                   (run-test-with-ui empty-test-suite :verbose 'silent)))
   (assert-output ""
                  (lambda ()
                    (run-test-with-ui empty-test-suite)))
@@ -23,10 +28,25 @@
 (define (test-empty-test-case)
   (assert-output ""
                  (lambda ()
+                   (run-test-with-ui empty-test-case :verbose 'silent)))
+  (assert-output ""
+                 (lambda ()
                    (run-test-with-ui empty-test-case)))
   (assert-output "-- (test case) empty test case:\n"
                  (lambda ()
                    (run-test-with-ui empty-test-case :verbose 'verbose)))
+  #f)
+
+(define (test-empty-test)
+  (assert-output ""
+                 (lambda ()
+                   (run-test-with-ui empty-test :verbose 'silent)))
+  (assert-output "."
+                 (lambda ()
+                   (run-test-with-ui empty-test)))
+  (assert-output "--- (test) empty test: .\n"
+                 (lambda ()
+                   (run-test-with-ui empty-test :verbose 'verbose)))
   #f)
 
 (provide "test/test-base")
