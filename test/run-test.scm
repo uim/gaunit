da79c569e1df9d20d50fc93e8bf233f1b9fb6c1c
Kouhei Sutou <kou@cozmixng.org>
2008-06-03 11:28:07 +0000

    * use sys-glob as fallback.

--- test/run-test.scm
+++ test/run-test.scm
@@ -2,17 +2,11 @@
 
 (add-load-path "./lib")
 
-(use gauche.interactive)
-(use file.util)
 (use test.unit)
 
-(if (and (symbol-bound? 'main)
-         (not (symbol-bound? '_main)))
-  (define _main main))
-
-(define (main args)
-  (let ((dir (sys-dirname (car args))))
-    (for-each load (glob #`",|dir|/**/test-*.scm"))
-    (if (symbol-bound? '_main)
-      (_main `(,(car args) "-vn" ,@(cdr args)))
-      (run-all-test))))
+(define base-dir (sys-dirname *program-name*))
+(for-each load
+          (if (symbol-bound? 'glob)
+            (glob #`",|base-dir|/**/test-*.scm")
+            (append (sys-glob #`",|base-dir|/test-*.scm")
+                    (sys-glob #`",|base-dir|/*/test-*.scm"))))
