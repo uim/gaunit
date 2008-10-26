a054a908d653753ed4e4644107a43977e94824fa
Kouhei Sutou <kou@cozmixng.org>
2008-10-26 10:59:28 +0000

    * improve path configuration.

--- test/run-test.scm
+++ test/run-test.scm
@@ -1,7 +1,18 @@
 #!/usr/bin/env gosh
 
-(add-load-path "./lib")
-(add-load-path ".")
+(use file.util)
+
+(define (gaunit-test-build-path . components)
+  (let* ((test-dir (sys-dirname *program-name*))
+         (top-dir (sys-realpath (build-path test-dir ".."))))
+    (apply build-path top-dir components)))
+
+(define-macro (%add-top-path-to-load-path)
+  `(add-load-path ,(gaunit-test-build-path)))
+(define-macro (%add-lib-path-to-load-path)
+  `(add-load-path ,(gaunit-test-build-path "lib")))
+(%add-lib-path-to-load-path)
+(%add-top-path-to-load-path)
 
 (use test.unit)
 
